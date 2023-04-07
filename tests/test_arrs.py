77496d8483c1165890032de4a9ecf5af8e3061b1
Vlad <sapozhnikov.061098@mail.ru>
2023-04-07 15:01:33 +0300

    Добавлен файл dict и функция с тестами к нему, использовась методика TDD, покрытие тестами 100%

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,4 +1,5 @@
 from utils import arrs
+from utils import dicts
 
 
 def test_get():
@@ -12,3 +13,11 @@ def test_slice():
     assert arrs.my_slice([], 0) == []
     assert arrs.my_slice([1, 2, 3], -6) == [1, 2, 3]
     assert arrs.my_slice([1, 2, 3, 4, 5], -2) == [4, 5]
+
+
+def test_get_val():
+    data = {"vcs": "mercurial"}
+    assert dicts.get_val(data, 'vcs') == 'mercurial'
+    assert dicts.get_val(data, 'vcs', 'git') == 'mercurial'
+    assert dicts.get_val({}, 'vcs', 'git') == 'git'
+    assert dicts.get_val({}, 'vcs', 'bazaar') == 'bazaar'
